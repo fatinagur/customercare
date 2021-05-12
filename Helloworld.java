6fc61edb07e963793ab1a47a03ea02039199ca54
Nagur vali <nagur.abc@gmail.com>
2021-05-12 06:37:58 +0000

    added facebook int project

--- Helloworld.java
+++ Helloworld.java
@@ -1,9 +1,9 @@
 public class HelloWorld
 {
     // Your program begins with a call to main().
-    // Prints "Hello, World" to the terminal window.
+    // Prints "Welcome to face book to the terminal window.
     public static void main(String args[])
     {
-        System.out.println("Hello, World");
+        System.out.println("welcome to facebook");
     }
 }
